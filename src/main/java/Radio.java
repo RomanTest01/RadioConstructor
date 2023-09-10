0ba10e5a731356a0640185ca44f90e04894b474f
Roman <borisenko0893@gmail.com>
2023-09-10 11:05:11 +0300

    add Radio and TestRadio

---
+++ src/main/java/Radio.java
@@ -0,0 +1,95 @@
+public class Radio {
+
+    private int lastRadioStation = 9;
+    private int currentStation;
+    private int increaseVolume;
+    private int maxVoidRadio = 100;
+
+    private int minVoidRadio = 0;
+
+
+
+
+    public Radio() {
+        this.lastRadioStation = 9;
+    }
+
+    public Radio( int stationsCount){
+        this.lastRadioStation = stationsCount - 1;
+
+    }
+
+
+    public int getCurrentStation () {
+
+        return currentStation;
+    }
+
+
+    public int getLastRadioStation () {
+        return lastRadioStation;
+    }
+
+    public int getIncreaseVolume () {
+        return increaseVolume;
+    }
+
+    public int getMaxVoidRadio () {
+        return maxVoidRadio;
+    }
+
+    public int getMinVoidRadio () {
+        return minVoidRadio;
+    }
+
+    public void setCurrentStation ( int newCurrentStation){  //выбор станции
+        if (newCurrentStation > lastRadioStation) {
+            newCurrentStation = 0;
+        }
+
+
+        currentStation = newCurrentStation;
+    }
+
+    public void setNextRadioStation () {   //переключение станции вперед
+        int next = currentStation + 1;
+
+        currentStation = next;
+        if (currentStation > lastRadioStation) {
+            currentStation = 0;
+        }
+
+    }
+
+    public void setPrevRadioStation () {  //переключение станции назад
+        int prev = currentStation - 1;
+
+        currentStation = prev;
+        if (currentStation < 0) {
+            currentStation = lastRadioStation;
+        }
+    }
+
+    public void setToMaxStation () {
+        currentStation = lastRadioStation;
+    }
+
+
+    public void setIncreaseVolume ( int newIncreaseVolume){  //выбор громкости
+        if (newIncreaseVolume > maxVoidRadio) {
+            newIncreaseVolume = maxVoidRadio;
+        }
+        if (newIncreaseVolume < minVoidRadio) {
+            newIncreaseVolume = minVoidRadio;
+        }
+
+
+        increaseVolume = newIncreaseVolume;
+    }
+
+    public void setMaxVolume () {
+        increaseVolume = maxVoidRadio;
+    }
+
+
+}
---
+++ src/test/java/RadioTest.java
@@ -0,0 +1,168 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class RadioTest {
+
+    Radio station = new Radio();
+    Radio volume = new Radio();
+
+
+    @Test
+    public void test() {
+        Radio station = new Radio(20);
+        station.setCurrentStation(15);
+
+        int exepted = 15;
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(exepted, actual);
+
+
+    }
+
+    @Test
+    public void testStation() {
+
+
+        Assertions.assertEquals(9, station.getLastRadioStation());
+    }
+
+    @Test
+    public void shouldStation() {
+
+
+        station.setCurrentStation(3);
+
+        int expected = 3;
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldMaxStation() {
+
+
+        station.setToMaxStation();
+
+        int expected = 9;
+        int actual = station.getLastRadioStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldStationAboveMax() {
+
+
+        station.setCurrentStation(10);
+
+        int expected = 0;
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNextRadioStation() {
+
+        station.setCurrentStation(9);
+
+        station.setNextRadioStation();
+
+        int expected = 0;
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldNextNotMoreMaxStation() {
+
+        station.setCurrentStation(7);
+
+        station.setNextRadioStation();
+
+        int expected = 8;
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldPrevRadioStation() {
+
+        station.setCurrentStation(-1);
+
+        station.setPrevRadioStation();
+
+
+        int expected = 9;
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldPrevNotMinStation() {
+
+        station.setCurrentStation(2);
+
+        station.setPrevRadioStation();
+
+        int expected = 1;
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+    //тесты на громкость
+
+    @Test
+    public void shouldVolume() {
+
+
+        station.setIncreaseVolume(15);
+
+        int expected = 15;
+        int actual = station.getIncreaseVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void choiceMaxVolume() {
+
+
+        volume.setMaxVolume();
+
+        int expected = 100;
+        int actual = volume.getMaxVoidRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldMoreMaxVolume() {
+
+
+        volume.setIncreaseVolume(102);
+
+        int expected = 100;
+        int actual = volume.getMaxVoidRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldMoreMinVolume() {
+
+
+        volume.setIncreaseVolume(-5);
+
+        int expected = 0;
+        int actual = volume.getMinVoidRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+}
